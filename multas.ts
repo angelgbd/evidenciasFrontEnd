0833767b3c2e2ec59d05e9e483bb7b04ac8951c2
angelgbd <baeltranangel@gmail.com>
2026-08-26 11:05:14 -0700

    Avance tarea

--- multas.ts
+++ multas.ts
@@ -1,6 +1,11 @@
+type EstadoPrestamo = "activo" | "vencido" | "devuelto";
+
 interface Prestamo{
+    folio: string;
     multa: number;
     ejemplar: number;
+    estado: EstadoPrestamo;
+    socio?: string; //el ? lo vuelve opcional: puede no venir 
 }
 
 function calcularMulta(prestamo: Prestamo): number {
@@ -8,6 +13,14 @@ function calcularMulta(prestamo: Prestamo): number {
     return prestamo.multa + cargoFijo;
 }
 
-const prestamo = {multa: 250, ejemplar: 15}; 
+function reciboDe(prestamo: Prestamo): string {
+    if(prestamo.socio == undefined){
+        return "No hay socio asignado";
+    }
+    return `Recibo para el socio: ${prestamo.socio.toUpperCase()} con multa de: ${calcularMulta(prestamo)}`;
+}
 
-console.log("Tu multa es de ", calcularMulta(prestamo));
+let prestamo: Prestamo = {multa: 250, ejemplar: 15, folio: "F001", estado: 'activo', socio: 'Juan Pérez'}; 
+
+console.log("Tu multa es de ", calcularMulta(prestamo));    
+console.log(reciboDe(prestamo));
